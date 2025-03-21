8fa3b1bf06f183e54bcb3f5bdce411b40aa7a874
Ben Wirf <ben.wirf@gmail.com>
2025-03-21 13:19:29 +0930

    Update test_process_gpkgs_in_order.py

--- Analysis_Scripts/test_process_gpkgs_in_order.py
+++ Analysis_Scripts/test_process_gpkgs_in_order.py
@@ -5,7 +5,7 @@ import os
 
 months_dict = dict((month, index) for index, month in enumerate(calendar.month_name) if month)
 
-homerange_source_folder = r'C:/Users/qw2/Desktop/Paddock_Power_GPS/home_range_geopackage_files'
+homerange_source_folder = r'Paddock_Power_GPS/home_range_geopackage_files'
 
 period_1_months = ['June', 'July', 'August', 'September', 'October', 'November', 'December']
 period_2_months = ['January', 'February', 'March', 'April', 'May']
